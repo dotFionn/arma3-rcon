0d213b82721e7d0c129e26fd33f7e4719ac5fb72
Fionn <fionn@fsperath.de>
2021-02-22 19:49:48 +0100

    created basic node-be wrapper

---
+++ app.js
@@ -0,0 +1,19 @@
+const be = require('node-be');
+
+function Arma3Rcon(ip, port, password) {
+  this.ip = ip;
+  this.port = port;
+  this.password = password;
+
+  this.connect = function () {
+    this.be = new be(this.ip, this.port, this.password);
+    this.be.connect();
+  }.bind(this);
+
+  this.close = function () {
+    this.be.close();
+    this.be = undefined;
+  }.bind(this);
+}
+
+module.exports = Arma3Rcon;
