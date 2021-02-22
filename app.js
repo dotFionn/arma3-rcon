38abb0bfbf835bb036bcae7cae7b2c7b8257ffe8
Fionn <fionn@fsperath.de>
2021-02-22 20:53:18 +0100

    replaced node-be library with battle-node

--- app.js
+++ app.js
@@ -1,20 +1,44 @@
-const be = require('node-be');
+const be = require('battle-node');
+  this.socket.unref();
+  this.socket.close();
+  cb();
+};
 
 function Arma3Rcon(ip, port, password) {
   this.ip = ip;
   this.port = port;
   this.password = password;
 
+  this.connectCallback = () => {};
+
   this.connect = function () {
     this.be = new be(this.ip, this.port, this.password);
-    this.be.connect();
+    return new Promise((res, rej) => {
+      this.be = new be({ ip: this.ip, port: this.port, rconPassword: this.password });
+
+      this.be.on('login', (err, success) => {
+        if (err) {
+          return rej(err);
+        }
+
+        res(success);
+      });
+
+      this.be.login();
+    });
   }.bind(this);
 
   this.close = function () {
-    if (this.be) {
-      this.be.close();
-    }
-    this.be = undefined;
+    return new Promise((res, rej) => {
+      try {
+        if (this.be) {
+          this.be.close(res);
+        }
+        this.be = undefined;
+      } catch (e) {
+        rej(e);
+      }
+    });
   }.bind(this);
 }
 
